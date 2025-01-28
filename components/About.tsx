c828996b70b2e667ec4110c814f8986e354e43d7
flukic <filiplukic000@gmail.com>
2025-01-28 13:32:24 -0500

    Remove unneeded comments

--- components/About.tsx
+++ components/About.tsx
@@ -19,59 +19,11 @@ const About: FC = () => {
             <SectionHeading>About me</SectionHeading>
             <div>
               <p>
-                {/* I am a{" "}
-                <span className="bold">
-                </span>{" "}
-                and a <span className="bold"></span>
-                {" "}
-                <span className="bold">
-                </span>
-                . I have honed my skills across{" "}
-                <span className="bold">
-                  various programming languages and technologies
-                </span>
-                , with a strong emphasis on{" "}
-                <span className="bold">
-                  web development, Python, Java, and C
-                </span>
-                . */}
-                I’m a Computer (Systems) Engineering graduate with high distinction from Carleton University.I’ve always worked hard and take an interest in my courses allowing me to achieve a GPA of 11.27/12.0 (3.91/4.0).
+                I’m a Computer (Systems) Engineering graduate with high distinction from Carleton University. I’ve always worked hard and taken an interest in my courses allowing me to achieve a GPA of 11.27/12.0 (3.91/4.0).
               </p>
               <br />
               <p>
-                {/* I am dedicated to{" "}
-                <span className="bold">continuous learning</span> and am always
-                seeking opportunities to{" "}
-                <span className="bold">
-                  enhance my skills while staying updated with the latest
-                  technology
-                </span>
-                . As a software engineer, I thrive for{" "}
-                <span className="bold">
-                  quality, efficiency, and innovation
-                </span>
-                . I also thrive in{" "}
-                <span className="bold">collaborative environments</span> where I
-                can{" "}
-                <span className="bold">
-                  contribute to and learn from a team
-                </span>
-                , driving projects to successful completion. If you'd like to
-                explore some of the projects I've worked on, feel free to check
-                out my{" "}
-                <Link
-                  href={externalLinks.github}
-                  className="bold"
-                  target="_blank"
-                >
-                  Github profile
-                </Link>
-                . I am actively{" "}
-                <span className="bold">
-                  seeking full-time software developer
-                </span>{" "}
-                positions to further advance my career. */}
-                 During my studies, I’ve completed 20 months of co-op experience among 4 different companies. I’m proficient with many programing languages with professional experience using JavaScript, TypeScript, C/C++, Python, and Java and love learning new things. I'm currently looking for full-time software developer positions.
+                 During my studies, I’ve completed 20 months of co-op experience among 4 different companies. I’m proficient with many programing languages, with professional experience using JavaScript, TypeScript, C/C++, Python, and Java and love learning new things. I'm currently looking for full-time software developer positions.
               </p>
             </div>
           </div>
